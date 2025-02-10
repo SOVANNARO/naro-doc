08ce5d72280dc6eac02090f13a2f005faff474f6
SOVANNARO <sovannaro066@gmail.com>
2025-02-11 00:06:52 +0700

    perf: :zap: add code google console

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type {ReactNode} from 'react';
+import type { ReactNode } from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -7,9 +7,10 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
+import Head from '@docusaurus/Head';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -30,11 +31,18 @@ function HomepageHeader() {
 }
 
 export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
+      <Head>
+        <meta name="google-site-verification" content="Cz26yDpb24keq5f9kHKZXdKVd8eI4rmvm8MOfwFrAsQ" />
+        <meta name="keywords" content="web development, front-end, back-end, full-stack, Angular, Vue.js, React, Spring Boot, Laravel, Node.js, Docker, Kubernetes, SQL, MongoDB, PostgreSQL, programming, coding, software engineering, developer learning, tutorials, API development" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="author" content="sovannaro" />
+        <meta name="description" content="Explore the journey of a web developer learning front-end, back-end, and DevOps technologies. A comprehensive guide for developers and learners." />
+      </Head>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
